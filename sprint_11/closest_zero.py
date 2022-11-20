283a89a3fcb7852140cf85b0277c080d68f6285b
Dmitriy Mikhalev <fakeforall@mail.ru>
2022-11-20 11:02:02 +0500

    Add type annotations, docstrings, alphabet sort. Rename files using PEP8. Add .gitignore. Codereview loop 1.

--- sprint_11/closest_zero.py
+++ sprint_11/closest_zero.py
@@ -1,15 +1,58 @@
+"""
+The first line contains the length of the street -- N (1 <= N <= 10^6).
+The next line contains N non—negative integers - house numbers and designations
+of empty areas on the map (zeros). It is guaranteed that there is at least one
+zero in the sequence. House numbers (positive numbers) are unique and do not
+exceed 10^9.
+
+For each of the sections, print the distance to the nearest zero. Print the
+numbers in one line, separating them with spaces.
+
+-------------+---------------+
+Sample input | Sample output |
+5            | 0 1 2 1 0     |
+0 1 4 9 0    |               |
+-------------+---------------+
+6            | 0 1 2 3 4 5   |
+0 7 9 4 8 20 |               |
+-------------+---------------+
+"""
 import sys
 
 
-def main():
-    size = int(sys.stdin.readline())
-    arr = list(map(int, sys.stdin.readline().split(' ')))
-    for i in get_zero_range(arr, size):
-        print(i, end=' ')
+def get_zero_range(arr: list[int], size: int) -> list[int]:
+    """
+    Get solution to the problem.
+    Required params:
+        arr: list[int] -- list of ints (area's number, has at least one 0)
+        size: int -- size of list
+    Returns a new list (given arr wasn't changed) of ints as the problem's
+    solution.
 
+    Algorithm:
+    1. Create a new array of 0 with the same size.
 
-def get_zero_range(arr, size):
-    res = [0 for _ in range(0, size)]
+    2. Create pointers to the left and right zero indexes. Both of them
+       are always defined (array has at least one 0). At start they have equal
+       value - first 0 index at array.
+
+    3. Bypass the given array by linear traversal:
+       If value is 0, left pointer is moving to this index and right pointer
+       is moving right to the first 0 value (and then cycle stops) or
+       to the end of array (out of values), then right pointer sets to None.
+
+       Else if value isn't 0, and right pointer isn't None, the result value at
+       this index sets to minimum of the modules of the difference between the
+       current index and the left and right pointers (distance to the near 0).
+
+       Else (value is not 0, right pointer is None) the last 0 at array (the
+       closest one to all next values) is behind the current index, so result
+       value at this index sets to difference between this index and left
+       pointer.
+
+    4. Return new array.
+    """
+    res = [0] * size
     right_zero_index = left_zero_index = arr.index(0)
 
     for i in range(0, size):
@@ -18,14 +61,39 @@ def get_zero_range(arr, size):
             while True:
                 right_zero_index += 1
                 if right_zero_index == size:
-                    right_zero_index = -1
+                    right_zero_index = None
                     break
                 elif arr[right_zero_index] == 0:
                     break
-        else:
+        elif right_zero_index is not None:
             res[i] = min(abs(i - left_zero_index), abs(right_zero_index - i))
+        else:
+            res[i] = i - left_zero_index
 
     return res
+
+
+def main() -> None:
+    """
+    The main function in which incoming data is read and transmitted to
+    receive a solution to the problem into output.
+    Returns None.
+    """
+    args: tuple[list[int], int] = read_data()
+    sys.stdout.write(
+        ' '.join([str(elem) for elem in get_zero_range(*args)])
+    )
+
+
+def read_data() -> tuple[list[int], int]:
+    """
+    Read data from stdin stream.
+    Returns a tuple with array of ints and its' size, for example ([-1, 4], 2).
+    """
+    size = int(sys.stdin.readline())
+    arr = [int(elem) for elem in sys.stdin.readline().split(' ')]
+
+    return arr, size
 
 
 if __name__ == '__main__':
---
+++ sprint_11/hands_sleight.py
@@ -0,0 +1,99 @@
+"""
+The game "Simulator for high-speed printing" is a field of 4x4 keys. In it,
+a configuration of numbers and dots appears on each round. Either a dot or
+a number from 1 to 9 is written on the key.
+
+At time T, the player must simultaneously press all the keys on which the
+number T is written. Gosha and Timofey can press K keys each at one time.
+If all the necessary keys are pressed at time T, then the players receive
+1 point.
+
+Find the number of points that Gosha and Timofey can earn if they press
+the keys together.
+
+The first line contains an integer K (1 <= K <= 5).
+The next 4 lines specify the type of simulator -- 4 characters in each line.
+Each character is either a dot or a digit. The characters of one
+line are consecutive and are not separated by spaces.
+
+Print a single number - the maximum number of points that 2 players can score.
+
+-------------+---------------+
+Sample input | Sample output |
+3            | 2             |
+1231         |               |
+2..2         |               |
+2..2         |               |
+2..2         |               |
+-------------+---------------+
+4            | 1             |
+1111         |               |
+9999         |               |
+1111         |               |
+9911         |               |
+-------------+---------------+
+4            | 0             |
+1111         |               |
+1111         |               |
+1111         |               |
+1111         |               |
+-------------+---------------+
+
+*You have to get count of digits which less or equal to 2 * K - for example,
+players can't press 13 times '4' button if they have 8 'fingers' together at
+T = 4 moment.
+"""
+import sys
+
+
+def get_scores(field: str, fingers_count: int) -> int:
+    """
+    Get solution to the problem.
+    Required params:
+        field: str -- string of field (concatenated lines)
+        fingers_count: int -- count of buttons that can be pushed
+    Returns scores -- integer.
+
+    Algorithm:
+    1. If count of digit at field is less or equal to fingers_count players
+    can push every button and earn 1 score -- update counter.
+    2. Do step #1 for every digit.
+    3. Return scores.
+    """
+    scores = 0
+    for digit in '123456789':
+        if 0 < field.count(digit) <= fingers_count:
+            scores += 1
+
+    return scores
+
+
+def main() -> None:
+    """
+    The main function in which incoming data is read and transmitted to
+    receive a solution to the problem into output.
+    Returns None.
+    """
+    fingers_count, field = read_data()
+    scores = get_scores(
+        field=field,
+        fingers_count=fingers_count*2
+    )
+    sys.stdout.write(str(scores))
+
+
+def read_data() -> tuple[int, str]:
+    """
+    Read 5 lines from stdin stream. Returns a tuple with int and str
+    For example, (1, '....\n....\n....\n....\n').
+    """
+    fingers_count = int(sys.stdin.readline())
+    field = ''
+    for _ in range(0, 4):
+        field += sys.stdin.readline()
+
+    return fingers_count, field
+
+
+if __name__ == '__main__':
+    main()
