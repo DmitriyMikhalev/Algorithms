03aa8478bd0c1612da0b578008e4fcf7dd8dc0b7
Dmitriy Mikhalev <fakeforall@mail.ru>
2023-02-03 19:59:34 +0500

    Add max tree value

---
+++ another algorithms/trees/lamps.py
@@ -0,0 +1,35 @@
+"""Get max value from tree."""
+LOCAL = False
+
+if LOCAL:
+    class Node:
+        def __init__(self, value, left=None, right=None):
+            self.value = value
+            self.right = right
+            self.left = left
+
+
+def solution(tree) -> int:
+    values = []
+
+    def foo(node, values):
+        if node:
+            values.append(node.value)
+            foo(node.left, values)
+            foo(node.right, values)
+
+    foo(tree, values)
+
+    return max(values)
+
+
+def test():
+    node1 = Node(1)
+    node2 = Node(-5)
+    node3 = Node(3, node1, node2)
+    node4 = Node(2, node3, None)
+    assert solution(node4) == 3
+
+
+if __name__ == '__main__':
+    test()
